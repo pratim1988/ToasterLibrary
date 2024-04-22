54848760385015108dc342c89428efa95c8ec1f5
pratim.sahoo <pratim.sahoo@tpwesternodisha.com>
2024-04-22 13:21:34 +0530

    Initial

---
+++ ToasterLibrary/src/main/java/com/tpodisha/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,12 @@
+package com.tpodisha.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void sPrint(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
